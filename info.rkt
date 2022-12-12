c0cd0f85b88e6bbd571c29739d183b75cae3c5d6
Daniel Patterson <dbp@dbpmail.net>
2022-12-12 13:47:37 -0500

    initial commit

---
+++ info.rkt
@@ -0,0 +1,7 @@
+#lang info
+
+(define collection "isl-spec")
+(define deps '("base" "htdp-lib"))
+(define pkg-desk "Intermediate Student with Lambda (ISL+) Enhanced with Specification support via Quickcheck")
+(define version "0.1")
+(define pkg-authors '("Daniel Patterson <dbp@dbpmail.net>"))
---
+++ main.rkt
@@ -0,0 +1,68 @@
+#lang racket
+
+(require (except-in lang/htdp-intermediate-lambda lambda require))
+(require (only-in lang/htdp-intermediate-lambda
+                    [lambda intermediate-lambda]
+                    [require intermediate-require]))
+
+ ; have to use this version of quickcheck because of restrictions in the test engine
+(require (only-in deinprogramm/quickcheck/quickcheck
+                  property
+                  ==>
+                  quickcheck-results
+                  check-result?
+                  arbitrary-integer
+                  arbitrary-boolean
+                  arbitrary-printable-ascii-string
+                  arbitrary-integer-from-to
+                  arbitrary-natural
+                  arbitrary-list
+                  arbitrary-nonempty-list
+                  arbitrary-record
+                  arbitrary-procedure))
+
+(require (only-in test-engine/test-engine add-test! 
+                                          add-failed-check! 
+                                          failed-check
+                                          property-fail))
+(require (only-in racket/syntax-srcloc syntax-srcloc))
+
+
+(provide (all-from-out lang/htdp-intermediate-lambda))
+(provide for-all ==> check-property)
+(provide (rename-out [intermediate-lambda lambda]
+                     [intermediate-require require]
+                     [arbitrary-integer gen:Integer]
+                     [arbitrary-printable-ascii-string gen:String]
+                     [arbitrary-boolean gen:Boolean]
+                     [arbitrary-integer-from-to gen:Integer-from-to]
+                     [arbitrary-natural gen:Natural]
+                     [arbitrary-list gen:ListOf]
+                     [arbitrary-nonempty-list gen:NonEmptyListOf]
+                     [arbitrary-record gen:RecordOf]
+                     [arbitrary-procedure gen:ProcedureOf]))
+
+
+
+(module reader syntax/module-reader
+  isl-spec)
+
+(define-syntax for-all
+  (syntax-rules ()
+    [(for-all ((?id ?gen) ...) ?body0 ?body1 ...)
+     (property ((?id ?gen) ...) ?body0 ?body1 ...)]))
+
+(define (do-check-property src prop)
+  (add-test! (lambda ()
+               (let-values ([(ntest stamps result) (quickcheck-results prop)])
+                 (if (check-result? result)
+                     (begin (add-failed-check! (failed-check (property-fail (syntax-srcloc src) result) (syntax-srcloc src)))
+                            #f)
+                     #t)))))
+
+(define-syntax (check-property stx)
+  (unless (memq (syntax-local-context) '(module top-level))
+    (raise-syntax-error #f "`check-property' must be at the top level" stx))
+  (syntax-case stx ()
+    ((_ prop) #'(do-check-property #`stx prop))
+    (_ (raise-syntax-error #f "`check-property' expects a single operand" stx))))
