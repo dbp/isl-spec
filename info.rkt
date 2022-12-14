4d3cf86ced57ff023c5ca4b022732d9cf2a80084
Daniel Patterson <dbp@dbpmail.net>
2022-12-14 09:26:25 -0500

    Add missing dependency

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 
 (define collection "isl-spec")
-(define deps '("base" "htdp-lib"))
+(define deps '("base" "htdp-lib" "deinprogramm-signature"))
 (define pkg-desk "Intermediate Student with Lambda (ISL+) Enhanced with Specification support via Quickcheck")
 (define version "0.1")
 (define license 'MIT)
