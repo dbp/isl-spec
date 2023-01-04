6210242482374230ede94313d6de0f9b1593408f
Daniel Patterson <dbp@dbpmail.net>
2023-01-04 17:38:55 -0500

    add quickcheck dependency

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 
 (define collection "isl-spec")
-(define deps '("base" "htdp-lib" "deinprogramm-signature"))
+(define deps '("base" "htdp-lib" "deinprogramm-signature" "quickcheck"))
 (define pkg-desk "Intermediate Student with Lambda (ISL+) Enhanced with Specification support via Quickcheck")
 (define version "0.1")
 (define license 'MIT)
